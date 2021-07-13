6fcceae52b8f3bf00bb1005a1db861be539e5845
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-13 14:40:59 +0200

    Verified by Marti4n v1.0.4 js 30%

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+const video = document.getElementById("video")
+const play = document.getElementById("play")
+const stop = document.getElementById("stop")
+const progress = document.getElementById("progress")
+const timestamp = document.getElementById("timestamp")
 
+
+
+
+// Play & pause video
+const toggleVideoStatus = () => {
+  if(video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
+
+
+
+// Update play/pause icon
+const updatePlayIcon = () => {
+  return true
+}
+
+
+
+// Update progress & timestamp
+const updateProgress = () => {
+  return true
+}
+
+
+
+ // Set video time to progress
+const setVideoProgress = () => {
+  return true
+}
+
+
+// Set stop
+const stopVideo = () => {
+  return true
+}
+
+
+
+video.addEventListener("click", toggleVideoStatus)
+video.addEventListener("pause", togglePlayIcon)
+video.addEventListener("play", togglePlayIcon)
+video.addEventListener("click", updateProgress)
+
+play.addEventListener("click", toggleVideoStatus)
+
+stop.addEventListener("click", stopVIdeo)
+
+progress.addEventListener("change", setVideoProgress)
